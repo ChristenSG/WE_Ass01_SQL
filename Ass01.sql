7aab41c1ebcf93729c9202ba1737ee1f70f53405
Christen <christen.srghaly@yahoo.com>
2024-05-27 10:14:52 -0400

    Update Ass01.sql with change the phone number for Christen.

--- Ass01.sql
+++ Ass01.sql
@@ -8,4 +8,4 @@ CREATE TABLE Students (
 
 INSERT INTO Students (sin, name, phoneNum, email, courseName) Values (215423698, 'Jonathan', 589654235, 'jonathan@gmail.com', 'Networking');
 INSERT INTO Students (sin, name, phoneNum, email, courseName) Values (254125369, 'Mary', 587236951, 'mary@gmail.com', 'Software Enginering');
-INSERT INTO Students (sin, name, phoneNum, email, courseName) Values (255425369, 'Christen', 587236852, 'christen@gmail.com', 'Web Design');
+INSERT INTO Students (sin, name, phoneNum, email, courseName) Values (258425369, 'Christen', 587236852, 'christen@gmail.com', 'Web Design');
